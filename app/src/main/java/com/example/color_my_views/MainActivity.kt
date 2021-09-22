6f49753e579c4816d6dd7cfe8ce6c102a8801764
Abhishek Jha <abhishek.agc966@gmail.com>
2021-09-22 15:04:09 +0530

    Added Buttons and implemented Baseline constraints

--- app/src/main/java/com/example/color_my_views/MainActivity.kt
+++ app/src/main/java/com/example/color_my_views/MainActivity.kt
@@ -20,7 +20,8 @@ class MainActivity : AppCompatActivity() {
     private fun setlisteners() {
 
        val clickableView:List<View> =
-           listOf(boxOneTextt,boxTwoText,boxThreeText,boxFourText,boxFiveText,constraintLayout1)
+           listOf(boxOneTextt,boxTwoText,boxThreeText,boxFourText,boxFiveText,constraintLayout1,redButton
+           ,greenButton,yellowButton)
 
         for(item in clickableView){
             item.setOnClickListener{makecolored(it)}
@@ -35,9 +36,12 @@ class MainActivity : AppCompatActivity() {
         when(view.id){
             R.id.boxOneTextt->view.setBackgroundColor(Color.DKGRAY)
             R.id.boxTwoText->view.setBackgroundColor(Color.GRAY)
-            R.id.boxThreeText->view.setBackgroundColor(Color.BLUE)
-            R.id.boxFourText->view.setBackgroundColor(Color.YELLOW)
-            R.id.boxFiveText->view.setBackgroundColor(Color.CYAN)
+            R.id.boxThreeText->view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.boxFourText->view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.boxFiveText->view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.redButton->boxThreeText.setBackgroundResource(R.color.myRed)
+            R.id.yellowButton->boxFourText.setBackgroundResource(R.color.myYellow)
+            R.id.greenButton->boxFiveText.setBackgroundResource(R.color.myGreen)
 
             else ->view.setBackgroundColor(Color.LTGRAY)
         }
